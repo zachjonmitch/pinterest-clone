41131a3fa2c22cfa44e3e2c53c45129904cd80ea
Zach Mitchell <zjmitche@gmail.com>
2017-08-18 09:57:49 -0600

    Navigation bar

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,56 +1,50 @@
 import React, { Component } from 'react';
 
+import NavigationBar from './NavigationBar.js';
+
 class Dashboard extends Component {
     render() {
         return (
             <main id="dashboard">
-               <nav className="masthead">
-                    <div className="masthead__brand"></div>
-                    <ul className="masthead__links">
-                        <li className="masthead__link"></li>
-                        <li className="masthead__link"></li>
-                    </ul>
-                    <form className="masthead__form">
-                        <input className="masthead__input--search" type="search"/>
-                    </form>
-                    <div className="masthead__icons">
-                        <div className="masthead__icon masthead--compass"></div>
-                        <div className="masthead__icon masthead--add"></div>
-                        <div className="masthead__icon masthead--alerts"></div>
-                        <div className="masthead__icon masthead--avatar"></div>
-                    </div>
-                    <div className="masthead__mobiles">
-                        <div className="masthead__mobile">
-                            <span className="masthead__mobile--brand fa fa-pinterest"></span>
-                        </div>
-                        <div className="masthead__mobile">
-                            <span className="masthead__mobile--search fa fa-search"></span>
-                        </div>
-                        <div className="masthead__mobile">
-                            <span className="masthead__mobile--menu fa fa-bars"></span>
-                        </div>
-                        <div className="masthead__mobile">
-                            <span className="masthead__mobile--user fa fa-user"></span>
-                        </div>
-                        <div className="masthead__mobile">
-                            <span className="masthead__mobile--notifications fa fa-bell"></span>
-                        </div>
-                    </div>
-               </nav>
+                <NavigationBar />
+                
+                <section className="masonry">
+                    <div className="masonry__cards">
 
-                <section className="posts">
-                    <div className="posts__cards">
-                        <img src="" alt=""/>
-                        <div className="posts__content">
-                            <h2 className="posts__title"></h2>
-                            <img src="" alt="" className="posts__avatar"/>
-                            <p className="posts__author"></p>
-                            <p className="posts__category"></p>
+                        <div className="masonry__card">
+                            <div className="masonry__content">
+                                <img src="" alt=""/>
+                                <h2 className="masonry__title"></h2>
+                                <img src="" alt="" className="masonry__avatar"/>
+                                <p className="masonry__author"></p>
+                                <p className="masonry__category"></p>
+                            </div>
                         </div>
+
+                        <div className="masonry__card">
+                            <div className="masonry__content">
+                                <img src="" alt=""/>
+                                <h2 className="masonry__title"></h2>
+                                <img src="" alt="" className="masonry__avatar"/>
+                                <p className="masonry__author"></p>
+                                <p className="masonry__category"></p>
+                            </div>
+                        </div>
+
+                        <div className="masonry__card">
+                            <div className="masonry__content">
+                                <img src="" alt=""/>
+                                <h2 className="masonry__title"></h2>
+                                <img src="" alt="" className="masonry__avatar"/>
+                                <p className="masonry__author"></p>
+                                <p className="masonry__category"></p>
+                            </div>
+                        </div>
+
                     </div>
                 </section>
 
-                <sections id="notifications">
+                <sections className="notifications">
                     <div className="notifications__images">
                         <img src="" alt=""/>
                     </div>
--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -5,6 +5,19 @@ import { connect } from 'react-redux';
 import { logout } from '../actions/authActions.js';
 
 class NavigationBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { isModalOpen: false }
+    }
+
+    openModal() {
+        this.setState({ isModalOpen: true })
+    }
+
+    closeModal() {
+        this.setState({ isModalOpen: false })
+    }
+
     logout(e) {
         e.preventDefault();
         this.props.logout();
@@ -25,9 +38,60 @@ class NavigationBar extends Component {
             </ul>
         );
         return (
-            <div className="navigation">
-                { isAuthenticated ? userLinks : guestLinks }
-            </div>
+            <nav className="masthead">
+                <div className="masthead__brand">
+                    <span className="masthead__brand--pinterest fa fa-pinterest fa-2x"></span>
+                </div>
+                <ul className="masthead__links">
+                    <li className="masthead__link">Analytics</li>
+                    <li className="masthead__link">Ads</li>
+                </ul>
+                <form className="masthead__form">
+                    <input className="masthead__input--search" type="search" placeholder="search"/>
+                </form>
+                <div className="masthead__icons">
+                    <div className="masthead__icon">
+                        <span className="masthead__icon--compass fa fa-compass"></span>
+                    </div>
+                    <div className="masthead__icon">
+                        <span className="masthead__icon--add fa fa-plus" onClick={() => this.openModal()}></span>
+                    </div>
+                    <div className="masthead__icon">
+                        <span className="masthead__icon--alerts fa fa-bell"></span>
+                    </div>
+                    <div className="masthead__icon">
+                        <img src="" alt=""/>
+                    </div>
+                </div>
+
+                <div className="navigation">
+                    { isAuthenticated ? userLinks : guestLinks }
+                </div>
+
+                <Modal isOpen={this.state.isModalOpen} onClose={() => this.closeModal()}>
+                    <h1>Modal title</h1>
+                    <p>hello</p>
+                    <p><button onClick={() => this.closeModal()}>Close</button></p>
+                </Modal>
+
+                <div className="masthead__mobiles">
+                    <div className="masthead__mobile">
+                        <span className="masthead__mobile--brand fa fa-pinterest"></span>
+                    </div>
+                    <div className="masthead__mobile">
+                        <span className="masthead__mobile--search fa fa-search"></span>
+                    </div>
+                    <div className="masthead__mobile">
+                        <span className="masthead__mobile--menu fa fa-bars"></span>
+                    </div>
+                    <div className="masthead__mobile">
+                        <span className="masthead__mobile--user fa fa-user"></span>
+                    </div>
+                    <div className="masthead__mobile">
+                        <span className="masthead__mobile--notifications fa fa-bell"></span>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
